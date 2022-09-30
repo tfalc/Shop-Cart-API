1ccc6b9bd44a4461b7fa4f21b1964b0161c0fe13
Thiago F <thiago.falcao86@gmail.com>
2022-09-30 13:11:18 -0300

    Create SacolaController.java

---
+++ sacola-api_gradle/src/main/java/com/tfalcon/sacola/resource/SacolaController.java
@@ -0,0 +1,37 @@
+package com.tfalcon.sacola.resource;
+
+import com.tfalcon.sacola.model.Item;
+import com.tfalcon.sacola.model.Sacola;
+import com.tfalcon.sacola.resource.dto.ItemDto;
+import com.tfalcon.sacola.service.SacolaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/sacola")
+@RequiredArgsConstructor
+public class SacolaController {
+
+    private final SacolaService sacolaService;
+
+    @RequestMapping(value = "/restest")
+    public String healthCheck(){
+        return "ok";
+    }
+
+    @GetMapping("/{id}")
+    public Sacola getSacolaById(@PathVariable("id") Long id){
+        return sacolaService.verSacola(id);
+    }
+
+    @PostMapping
+    public Item addItem(@RequestBody ItemDto itemDto){
+        return sacolaService.inserirItemSacola(itemDto);
+    }
+
+    @PatchMapping("/closebag/{sacolaId}")
+    public Sacola fecharSacola(@PathVariable("sacolaId") Long sacolaId,
+                               @RequestParam("formaPagamento") int formaPagamento) {
+        return sacolaService.fecharSacola(sacolaId, formaPagamento);
+    }
+}
