becc64dd5ed80d2657e8305f7c2112d980ca50e3
Thiago F <thiago.falcao86@gmail.com>
2022-09-30 13:48:05 -0300

    Update Item.java

--- sacola-api_gradle/src/main/java/com/tfalcon/sacola/model/Item.java
+++ sacola-api_gradle/src/main/java/com/tfalcon/sacola/model/Item.java
@@ -2,12 +2,14 @@ package com.tfalcon.sacola.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
