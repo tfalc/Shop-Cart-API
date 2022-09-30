a50b7ef1dbd4f5682b17c8b61e4466f4e5d35b3d
Thiago F <thiago.falcao86@gmail.com>
2022-09-30 13:48:08 -0300

    Update SacolaServiceImpl.java

--- sacola-api_gradle/src/main/java/com/tfalcon/sacola/service/impl/SacolaServiceImpl.java
+++ sacola-api_gradle/src/main/java/com/tfalcon/sacola/service/impl/SacolaServiceImpl.java
@@ -2,22 +2,63 @@ package com.tfalcon.sacola.service.impl;
 
 import com.tfalcon.sacola.enums.FormaPagamento;
 import com.tfalcon.sacola.model.Item;
+import com.tfalcon.sacola.model.Restaurante;
 import com.tfalcon.sacola.model.Sacola;
+import com.tfalcon.sacola.repository.ItemRepository;
+import com.tfalcon.sacola.repository.ProdutoRepository;
 import com.tfalcon.sacola.repository.SacolaRepository;
 import com.tfalcon.sacola.resource.dto.ItemDto;
 import com.tfalcon.sacola.service.SacolaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class SacolaServiceImpl implements SacolaService {
 
     private final SacolaRepository sacolaRepository;
+    private final ProdutoRepository produtoRepository;
+
+    private final ItemRepository itemRepository;
 
     @Override
     public Item inserirItemSacola(ItemDto itemDto) {
-        return null;
+
+        Sacola sacola = verSacola(itemDto.getIdSacola());
+
+        if(sacola.isFechada()){
+            throw new RuntimeException("Sacola está fechada");
+        }
+
+        Item itemParaInserir = Item.builder()
+                .quantidade(itemDto.getQuantidade())
+                .sacola(sacola)
+                .produto(produtoRepository.findById(itemDto.getProdutoId()).orElseThrow(
+                        () -> {
+                            throw new RuntimeException("Produto não encontrado");
+                        }
+                ))
+                .build();
+
+        List<Item> itens = sacola.getItens();
+        if(itens.isEmpty()){
+            itens.add(itemParaInserir);
+        }else{
+            Restaurante restaurante = itens.get(0).getProduto().getRestaurante();
+            Restaurante restauranteDoProduto = itemParaInserir.getProduto().getRestaurante();
+
+            if(restaurante.equals(restauranteDoProduto)){
+                itens.add(itemParaInserir);
+            }else {
+                throw new RuntimeException("Não foi possível adicionar itens na sacola. Feche a sacola atual.");
+            }
+        }
+
+        sacolaRepository.save(sacola);
+        
+        return itemRepository.save(itemParaInserir);
     }
 
     @Override
